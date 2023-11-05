7a190e558f24c5e8f4cbebc37570087a34e05818
Valentina Alzate <valzate618@gmail.com>
2023-11-06 00:36:31 +0100

    Update tailwind.config

---
+++ src/rickandmorty/assets/images/index.js
@@ -0,0 +1,4 @@
+export * from './bg-character.webp'
+export * from './desktop-d.jpg'
+export * from './desktop.jpg'
+export * from './laptop-d.jpg'
--- src/rickandmorty/components/MainCharactersPage.jsx
+++ src/rickandmorty/components/MainCharactersPage.jsx
@@ -1,6 +1,5 @@
 import { useRef } from "react";
 import PropTypes from 'prop-types'
-import {image} from '../assets/images/bg-character.webp'
 import { CharactersList, LoadCharactersOnIntersection, LoadingData, ResultsNotFound, useCharacters } from "../"
 
 
@@ -15,7 +14,6 @@ export const MainCharactersPage = ({list}) => {
     <>
       <div className="min-h-screen h-full dark:bg-zinc-800">
         <div className='bg-movil sm:bg-tablet lg:bg-laptop 2xl:bg-desktop bg-no-repeat bg-bottom bg-cover max-w-full h-[60vh] dark:bg-movil-d sm:dark:bg-tablet-d lg:dark:bg-laptop-d 2xl:dark:bg-desktop-d'></div>
-        <img src={image} />
         <div className="relative mx-auto -mt-40 min-w-[80%] max-w-[80%] min-h-[60vh] h-auto rounded-md lg:col-span-2 p-4 md:p-10 bg-white z-10 dark:bg-zinc-800">
           {!isLoading && <CharactersList characters={list} />}
           {
